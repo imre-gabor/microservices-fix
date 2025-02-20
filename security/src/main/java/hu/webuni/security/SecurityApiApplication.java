299601e525a18e61a493c4ff25288cb5818c43dc
imre-gabor <gabor@aut.bme.hu>
2025-02-20 12:49:34 +0100

    move SecurityApplication to security project from security-api

--- security/src/main/java/hu/webuni/security/SecurityApiApplication.java
+++ security/src/main/java/hu/webuni/security/SecurityApiApplication.java
@@ -1,4 +1,4 @@
-package hu.webuni.security.api;
+package hu.webuni.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
